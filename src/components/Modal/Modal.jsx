7ebd8477425014343eb0c2f8d688f5737b78f831
LysovMaxim <max_maran@icloud.com>
2023-05-21 12:33:04 +0300

    Переписав useEffect для модалки

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,14 +4,16 @@ import PropTypes from 'prop-types';
 
 export const Modal = ({ urlPhoto, onClose }) => {
   useEffect(() => {
+    const hendleKeyDown = event => {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+    };
     window.addEventListener('keydown', hendleKeyDown);
-  });
-
-  const hendleKeyDown = event => {
-    if (event.code === 'Escape') {
-      onClose();
-    }
-  };
+    return () => {
+      window.removeEventListener('keydown', hendleKeyDown);
+    };
+  }, [onClose]);
 
   const hendleBeckdropClick = event => {
     if (event.currentTarget === event.target) {
@@ -30,5 +32,5 @@ export const Modal = ({ urlPhoto, onClose }) => {
 
 Modal.propTypes = {
   onClose: PropTypes.func.isRequired,
-  urlPhoto: PropTypes.string.isRequired
+  urlPhoto: PropTypes.string.isRequired,
 };
